088e0a35ff36c9f1077189fdc609694149f24a6d
Shilpaj <shilpajbhalerao@gmail.com>
2024-12-20 07:00:33 +0530

    Feat: Completed logic using enum

--- library_system.py
+++ library_system.py
@@ -61,3 +61,37 @@ class Book:
         self.title = title
         self.genre = genre
         self.is_available = is_available
+
+    def borrow(self) -> None:
+        """
+        Method to borrow a book
+        """
+        if not self.is_available:
+            raise BookNotAvailableError("Book is not available")
+        self.is_available = False
+
+    def return_book(self, is_late: bool) -> None:
+        """
+        Method to return a book
+        """
+        if is_late:
+            raise LateReturnError("Book is late")
+        self.is_available = True
+
+
+class Member:
+    """
+    Member Class
+    """
+    def __init__(self, name: str, membership_level: MembershipLevel) -> None:
+        self.name = name
+        self.membership_level = membership_level
+
+    def get_fee(self) -> int:
+        """
+        Method to get the fee associated with membership level
+        """
+        # If membership level is invalid, raise an error
+        if self.membership_level not in MembershipLevel.__members__.values():
+            raise InvalidMembershipError("Invalid membership level")
+        return self.membership_level.value
